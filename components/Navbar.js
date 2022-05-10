f6deec7c2d71b9678c9320fc0e99abc5d5d848b5
Linus Kullman <linus.kullman@outlook.com>
2022-05-10 16:17:09 +0200

    accent added to nav

--- components/Navbar.js
+++ components/Navbar.js
@@ -3,9 +3,9 @@ export default function Navbar() {
     <div className="relative">
       <nav className="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-800">
         <div className="container flex flex-wrap items-start space-x-4 ">
-          <a className="text-white text-l hover:text-sky-700" href="#">Item1</a>
-          <a className="text-white text-m hover:font-accent" href="#">Item2</a>
-          <a className="text-white text-m hover:text-sky-700" href="#">Item3</a>
+          <a className="text-white text-l hover:text-accent/80" href="#">Item1</a>
+          <a className="text-white text-m hover:text-accent/80" href="#">Item2</a>
+          <a className="text-white text-m hover:text-accent/80" href="#">Item3</a>
         </div>
       </nav>
     </div>
