f5752df156dc0702201356954fa6e6eee749016d
Linus Kullman <linus.kullman@outlook.com>
2022-05-10 12:46:14 +0200

    styling index

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import GameItem from "../components/GameItem";
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
 
 export async function getStaticProps() {
 
   const res = await fetch('https://api.rawg.io/api/games?key=dc469c23c1bb4c1bbb5d9562b46e5082');
   const data = await res.json();
+
 
   return {
     props: { 
@@ -15,23 +17,34 @@ export async function getStaticProps() {
   }
 }
 
+
 export default function Home( {count, games, prevPage, nextPage} ) {
 
   return (
-      <main className="flex flex-col">
-          <div className="basis-2/5">
-            <h1>Games</h1>
+    
+      <main className="flex flex-col bg-zinc-900">
+        <div className="bg-sky-900/50">
+          <a className="active" href="">Home</a>
+        </div>
+        <div className="px-16 py-16">
+          <div className="py-8 text-center basis-2/5 font-rubrik">
+            <h1 class="text-slate-50 text-2xl">Games</h1>
+            <h4 class="text-sky-400 text-1xl">Featured in May</h4>
+          </div>
+          <div class="topnav text-center px-4 py-4">
+            <input type="text " placeholder="Search..."></input>
           </div>
           <div className="basis-1/2">
-            <div className="grid grid-cols-2 gap-4 basis-7/12">
-                {games.map((game) => (
-                  <GameItem key={game.id} game={game}></GameItem>
-                ))}
+            <div className="grid grid-cols-2 gap-8 basis-7/12">
+                  {games.map((game) => (
+                    <GameItem key={game.id} game={game}></GameItem>
+                  ))}
             </div>
           </div>
           <div className="basis-1/10">
-            <p>Page 1</p>
+            <p class="pt-16 font-roboto font-style: italic text-slate-50 text-center">Page 1</p>
           </div>
+        </div>
       </main>
   );
 }
