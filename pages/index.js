6d578d02590f15ee7724483d2a0db477c67d69a9
Sengil <samuel.nilsson842@gmail.com>
2022-05-10 09:12:02 +0200

    Feature: Added a GameItem components and some basic CSS to start with.

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,5 @@
+import GameItem from "../components/GameItem";
+
 export async function getStaticProps() {
 
   const res = await fetch('https://api.rawg.io/api/games?key=dc469c23c1bb4c1bbb5d9562b46e5082');
@@ -16,13 +18,20 @@ export async function getStaticProps() {
 export default function Home( {count, games, prevPage, nextPage} ) {
 
   return (
-      <main>
-        <h1 className="text-lg">We have {count} games.</h1> 
-          <ul>
-            {games.map((game) => (
-              <li key={game.id}>{game.name}</li>
-            ))}
-          </ul>
+      <main className="flex flex-col">
+          <div className="basis-2/5">
+            <h1>Games</h1>
+          </div>
+          <div className="basis-1/2">
+            <div className="grid grid-cols-2 gap-4 basis-7/12">
+                {games.map((game) => (
+                  <GameItem key={game.id} game={game}></GameItem>
+                ))}
+            </div>
+          </div>
+          <div className="basis-1/10">
+            <p>Page 1</p>
+          </div>
       </main>
   );
 }
