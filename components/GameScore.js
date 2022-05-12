d2e9933fad010409e1b432d5845c850d4478a1d9
Tobias Bergström <tobiasbergstrom@outlook.com>
2022-05-12 15:07:33 +0200

    refactor: refactored the code

--- components/GameScore.js
+++ components/GameScore.js
@@ -1,40 +1,28 @@
 export default function GameScore({ score }) {
   var highscore = 75;
   var lowscore = 50;
-  if (score >= highscore) {
-    return (
-      <div className="relative">
+  return (
+    <div className="relative">
+      {score >= highscore && (
         <div className="border inline-flex px-1 rounded-lg border-green-400 text-green-400 bg-green-400/[0.1] mt-1 mr-1">
           {score}
         </div>
-      </div>
-    );
-  }
-  if (score <= highscore && score >= lowscore) {
-    return (
-      <div className="relative">
+      )}
+      {score < highscore && score > lowscore && (
         <div className="border inline-flex px-1 rounded-lg border-yellow-400 text-yellow-400 bg-yellow-400/[0.1] mt-1 mr-1">
           {score}
         </div>
-      </div>
-    );
-  }
-  if (score <= lowscore && score > 0) {
-    return (
-      <div className="relative">
+      )}
+      {score < lowscore && (
         <div className="border inline-flex px-1 rounded-lg border-red-400 text-red-400 bg-red-400/[0.1] mt-1 mr-1">
           {score}
         </div>
-      </div>
-    );
-  }
-  if (score === null) {
-    return (
-      <div className="relative">
-        <div className="border inline-flex px-1 rounded-lg border-white text-white bg-white/[0.1] mt-1 mr-1">
+      )}
+      {score < 0 && (
+        <div className="border inline-flex px-1 rounded-lg border-white-400 text-white-400 bg-white-400/[0.1] mt-1 mr-1">
           ?
         </div>
-      </div>
-    );
-  }
+      )}
+    </div>
+  );
 }
