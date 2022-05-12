10d3cff121b0b8d081d953aacd8003261c5de0b5
Tobias Bergström <tobiasbergstrom@outlook.com>
2022-05-12 15:07:41 +0200

    style: styled footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,15 +1,22 @@
-import React from 'react'
+import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="p-4 bg-white shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800">
-        <ul className="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
-            <li>
-                <a href="#top">Back to top</a>
-            </li>
-        </ul>
-    </footer>
-  )
-}
+    <>
+      <footer className="p-4 bg-zinc-800 flex justify-between">
+        <a
+          className="text-white font-Roboto hover:text-accent mx-2"
+          href="#top"
+        >
+          Back to top
+        </a>
+        <div className="text-zinc-600 mx-2">
+          Made by <a>Samuel Nilsson</a>, <a>Linus Kullman</a> and{" "}
+          <a>Tobias Bergström</a>
+        </div>
+      </footer>
+    </>
+  );
+};
 
-export default Footer
+export default Footer;
