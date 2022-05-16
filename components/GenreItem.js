93df75a2e1168cfd56aa04703538a0e35665db33
Tobias Bergström <tobiasbergstrom@outlook.com>
2022-05-16 11:01:37 +0200

    fix: fixed render order

--- components/GenreItem.js
+++ components/GenreItem.js
@@ -5,14 +5,14 @@ export default function GenreItem({ genre }) {
       {toggled ? (
         <button
           onClick={() => (toggled = !toggled)}
-          className="m-1 text-accent font-Roboto border-2 border-accent rounded-md px-2 opacity-100 hover:opacity-50 transition duration-300"
+          className="m-1 text-zinc-800 font-Roboto rounded-md bg-accent px-2 opacity-100 hover:opacity-50 transition duration-300"
         >
           {genre}
         </button>
       ) : (
         <button
           onClick={() => (toggled = !toggled)}
-          className="m-1 text-zinc-800 font-Roboto rounded-md bg-accent px-2 opacity-100 hover:opacity-50 transition duration-300"
+          className="m-1 text-accent font-Roboto border-2 border-accent rounded-md px-2 opacity-100 hover:opacity-50 transition duration-300"
         >
           {genre}
         </button>
