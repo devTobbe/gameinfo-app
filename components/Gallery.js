33a34451b80174452a77c19363bb8f7d808d5e50
Linus Kullman <linus.kullman@outlook.com>
2022-05-23 11:56:17 +0200

    Gallery Shell

---
+++ components/Gallery.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+export default function Gallery({screenshots}){
+
+    var game_pk = "";
+    count = 0;
+    next = "<uri>";
+    previous = "<uri>";
+    results = [{screenshots}]; 
+
+    try {
+        const gameResponse = await fetch(
+          "https://api.rawg.io/api/games/"+game_pk+"/screenshots");
+        if (!gameResponse.ok) throw Error("Did not receive screenshots.");
+        const screenshots = await gameResponse.json();
+    } catch (err) {
+        console.log(err.message);
+    }
+/*
+    {
+        "count": 0,
+        "next": "http://example.com",
+        "previous": "http://example.com",
+        "results": [+{...}]
+    }
+*/
+    
+return(
+    <div>
+
+    </div>
+)
+
+}
